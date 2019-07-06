688b721e59c5561020ac563684b6686abd845a8f
Gian <gian22_02@hotmail.com>
2019-07-06 14:44:15 -0500

    Adding examples from synapse library

---
+++ Example04-TV/Logic/discover.js
@@ -0,0 +1,27 @@
+var listAvaliableServers = [];
+
+function entablishConnection(url) {
+  $(location).attr("href", "./Game/menu.html?url=" + url);
+}
+
+function avaliableServersToDiv(server) {
+  listAvaliableServers.push(server);
+  var addOptions =
+    '<button type="button" id="' +
+    (server + 2) +
+    '" class="buttonPr" onclick="entablishConnection(\'' +
+    server +
+    "');\" >" +
+    server +
+    "</button>";
+  console.log(server + "\n");
+  var div = document.getElementById("avaliableServers");
+  div.innerHTML += addOptions;
+}
+
+var Synapse = new window.Synapse(8080);
+
+function finServers() {
+  Synapse.findServers(15, 5000, avaliableServersToDiv, null);
+}
+setTimeout(finServers, 3000);
